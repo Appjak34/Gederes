ad5d10c8023d048e83b47f0e7919fdfdc0781e6a
Angel Emilio Lopez Fernandez <Appjak34@MacBook-Pro-de-Angel.local>
2017-04-23 22:26:05 -0500

    Se agrego el template de agregar platillos

---
+++ resources/views/templates/agregarPlatillos.blade.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="keywords" content="">
+    <meta name="description" content="">
+    
+    
+    <link rel="shortcut icon" type="image/png" href="favicon.png">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/animate.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
+    <link href='https://fonts.googleapis.com/css?family=helvetica&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+    
+    <script src="./js/jquery-2.1.0.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/blocs.min.js"></script>
+    <script src="./js/jqBootstrapValidation.js"></script>
+    <script src="./js/formHandler.js"></script>
+    <title>AgergarPlatillo</title>
+    <!-- Google Analytics -->
+    <!-- Google Analytics END -->
+</head>
+
+<body>
+    <!-- Main container -->
+    <div class="page-container">
+        <!-- bloc-14 -->
+        <div class="bloc bgc-white l-bloc " id="bloc-14">
+            <div class="container bloc-md">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div>
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="row">
+                                        <div class="col-sm-6">
+                                            <form id="form_18266" novalidate>
+                                                <div class="form-group"><label>Nombre<br></label><input class="form-control" id="input_227" /></div>
+                                                <div class="form-group"><label>Descripción<br></label><textarea class="form-control" rows="4" cols="50" id="textarea_20"></textarea></div>
+                                            </form>
+                                        </div>
+                                        <div class="col-sm-6"><img src="img/add_image.png" height="284" width="478" />
+                                            <div class="row">
+                                                <div class="col-sm-6">
+                                                    <form id="form_26192" novalidate>
+                                                        <div class="form-group"><label>Precio</label><input class="form-control" id="input_585" /></div>
+                                                    </form>
+                                                </div>
+                                                <div class="col-sm-6">
+                                                    <div class="radio"><label><input type="radio" name="radioSetOne" value="option1" />Choice one</label></div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- bloc-14 END -->
+        <!-- bloc-15 -->
+        <div class="bloc bgc-white l-bloc" id="bloc-15">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12"><a href="index.html" class="btn btn-wire btn-rd btn-xl wire-btn-light-salmon pull-right">+</a></div>
+                </div>
+            </div>
+        </div>
+        <!-- bloc-15 END -->
+    </div>
+    <!-- Main container END -->
+</body>
+
+</html>
